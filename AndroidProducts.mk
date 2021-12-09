53c6567745a5b5adb715f813550da30381d83c82
浅羽 <76060243+QianYuChan@users.noreply.github.com>
2021-12-09 18:03:39 +0800

    Initial
    Initial

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_XX.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_XX-user \
+    omni_XX-userdebug \
+    omni_XX-eng
